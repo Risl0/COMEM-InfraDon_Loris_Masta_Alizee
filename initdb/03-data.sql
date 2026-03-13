b0bc1f24b9203a6d317bb66940410400615379de
Matteo Stanca <matteo.stancataza@heig-vd.ch>
2026-03-13 16:03:07 +0100

    premiere requette data2

---
+++ initdb/03-data.sql
@@ -0,0 +1,16 @@
+
+-- Table Signalement
+SELECT
+    CASE
+        WHEN date ~ '^\d{2}\.\d{2}\.\d{4}$' 
+        THEN TO_CHAR(TO_DATE(date, 'DD.MM.YYYY'), 'YYYY-MM-DD')
+        ELSE date
+    END AS date_normalisee,
+    date AS date_originale,
+    signale_par,
+    objet,
+    description,
+    urgence,
+    statut
+FROM signalement
+ORDER BY date_normalisee;
